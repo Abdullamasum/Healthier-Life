4980ab942fc2c25337d8e1be496f66de0e70235f
juliusli <julius.eliel.liias@gmail.com>
2023-03-09 20:33:45 +0200

    Welcome screen video now shows up

--- views/Video.js
+++ views/Video.js
@@ -1,60 +1,22 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Button, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Video} from 'expo-av';
 import React from 'react';
 
 const MyVideo = () => {
-  const video = React.useRef(null);
   const secondVideo = React.useRef(null);
-  const [status, setStatus] = React.useState({});
-  const [statusSecondVideo, setStatusSecondVideo] = React.useState({});
 
   return (
     <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={setStatus}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title="Play from 5s"
-          onPress={() => video.current.playFromPositionAsync(5000)}
-        />
-        <Button
-          title={status.isLooping ? 'Set to not loop' : 'Set to loop'}
-          onPress={() => video.current.setIsLoopingAsync(!status.isLooping)}
-        />
-      </View>
       <Video
         ref={secondVideo}
         style={styles.video}
         source={require('../img/videoa_edit.mp4')}
         useNativeControls
         resizeMode="contain"
+        shouldPlay
         isLooping
-        onPlaybackStatusUpdate={setStatusSecondVideo}
       />
-      <View style={styles.buttons}>
-        <Button
-          title="Play from 50s"
-          onPress={() => secondVideo.current.playFromPositionAsync(50000)}
-        />
-        <Button
-          title={
-            statusSecondVideo.isLooping ? 'Set to not loop' : 'Set to loop'
-          }
-          onPress={() =>
-            secondVideo.current.setIsLoopingAsync(!statusSecondVideo.isLooping)
-          }
-        />
-      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -68,7 +30,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   video: {
-    flex: 1,
+    width: 300,
+    height: 300,
     alignSelf: 'stretch',
   },
   buttons: {
--- views/Welcome.js
+++ views/Welcome.js
@@ -94,6 +94,7 @@ const Welcome = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'purple',
   },
   text: {
     fontSize: 11,
