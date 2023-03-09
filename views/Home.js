e9df9b4d8eb741fac89a4e2027cf45a6e1f6dfc8
juliusli <julius.eliel.liias@gmail.com>
2023-03-09 19:09:37 +0200

    Removed blocking lottie icon from home view

--- views/Home.js
+++ views/Home.js
@@ -1,15 +1,12 @@
-import LottieView from 'lottie-react-native';
 import {Platform, StyleSheet, SafeAreaView} from 'react-native';
 import List from '../components/forms/List';
 import PropTypes from 'prop-types';
 
-import LottieIcons from '../components/forms/LottieIcons';
 const Home = ({navigation}) => {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <List navigation={navigation} />
-        <LottieView source={require('../Lottie/Home.json')} autoPlay />
       </SafeAreaView>
     </>
   );
